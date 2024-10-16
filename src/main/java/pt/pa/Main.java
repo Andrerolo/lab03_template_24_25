75116940e896fa89249e3ef80233f89c1c3ae7bd
Andrerolo <77112361+Andrerolo@users.noreply.github.com>
2024-10-16 12:54:58 +0100

    Nivel 1

--- src/main/java/pt/pa/Main.java
+++ src/main/java/pt/pa/Main.java
@@ -1,5 +1,8 @@
 package pt.pa;
 
+import pt.pa.adts.Position;
+import pt.pa.adts.TreeLinked;
+
 /**
  *
  * @author amfs
@@ -10,7 +13,61 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*
+        * Ecosystem
+              -Animals
+                -Mammals
+                  -Humans
+                  -Whales
+                -Birds
+                  -Eagles
+                  -Parrots
+              -Plants
+                -Trees
+                  -Oak
+                  -Pine
+                -Flowers
+                  -Rose
+                  -Tulip
 
+        * */
+
+        // Criação da instância TreeLinked com a raiz "Ecosystem"
+        TreeLinked<String> tree = new TreeLinked<>("Ecosystem");
+
+        // Adicionando nós à árvore de acordo com a Figura 1
+        Position<String> root = tree.root();
+
+        // Primeiro nível de filhos de "Ecosystem"
+        Position<String> animals = tree.insert(root, "Animals");
+        Position<String> plants = tree.insert(root, "Plants");
+
+        // Segundo nível - filhos de "Animals"
+        Position<String> mammals = tree.insert(animals, "Mammals");
+        Position<String> birds = tree.insert(animals, "Birds");
+
+        // Segundo nível - filhos de "Plants"
+        Position<String> trees = tree.insert(plants, "Trees");
+        Position<String> flowers = tree.insert(plants, "Flowers");
+
+        // Terceiro nível - filhos de "Mammals"
+        tree.insert(mammals, "Humans");
+        tree.insert(mammals, "Whales");
+
+        // Terceiro nível - filhos de "Birds"
+        tree.insert(birds, "Eagles");
+        tree.insert(birds, "Parrots");
+
+        // Terceiro nível - filhos de "Trees"
+        tree.insert(trees, "Oak");
+        tree.insert(trees, "Pine");
+
+        // Terceiro nível - filhos de "Flowers"
+        tree.insert(flowers, "Rose");
+        tree.insert(flowers, "Tulip");
+
+        // Imprimindo a árvore
+        System.out.println(tree.toString());
     }
 
 
---
+++ src/test/java/pt/pa/adts/TreeLinkedTest.java
@@ -0,0 +1,67 @@
+package pt.pa.adts;
+
+import junit.framework.TestCase;
+
+public class TreeLinkedTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testSize() {
+    }
+
+    public void testTestSize() {
+    }
+
+    public void testIsEmpty() {
+    }
+
+    public void testReplace() {
+    }
+
+    public void testRoot() {
+    }
+
+    public void testParent() {
+    }
+
+    public void testChildren() {
+    }
+
+    public void testIsInternal() {
+    }
+
+    public void testIsExternal() {
+    }
+
+    public void testIsRoot() {
+    }
+
+    public void testInsert() {
+    }
+
+    public void testTestInsert() {
+    }
+
+    public void testRemove() {
+    }
+
+    public void testPositions() {
+    }
+
+    public void testElements() {
+    }
+
+    public void testHeight() {
+    }
+
+    public void testMove() {
+    }
+
+    public void testIsAncestor() {
+    }
+
+    public void testTestToString() {
+    }
+}
